a4ab786e4db7f6312df2b9aa6dbbd5d2ac628ac6
voomdoon <nilreb24-github20141105@yahoo.de>
2024-02-29 20:07:09 +0100

    refactor: add main draft

---
+++ src/main/java/de/voomdoon/tool/projecttime/ProjectTimeMain.java
@@ -0,0 +1,12 @@
+package de.voomdoon.tool.projecttime;
+
+/**
+ * DOCME add JavaDoc for 
+ *
+ * @author André Schulz
+ *
+ * @since DOCME add inception version number
+ */
+public class ProjectTimeMain {
+
+}
